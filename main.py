af433f1484e3db7b0c88e25a9df276a378bced82
AdorableBuns <chuayuehan2000@hotmail.com>
2024-11-17 15:27:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+
+# variable = a container for a value . behaves as the values that value that it contains.
+
+
+#x = 6
+#x +=1
+#y = 'my age is '
+#print(y+str(x))
+
+#height= 173.05
+#print('Your  height is: '+str(height))
+#print(type(height))
+
+#human=True
+#print(type(human))
+#print("Are you human? "+str(human))
+
+#multiple assigmnent = allows us to assign multiple at the same time in one line of code
+
+#name = 'bro'
+#age = 21
+#attractive = True
+
+#name, age, attractive = 'bro', 21, True
+
+#print(name)
+#print(age)
+#print(attractive)
+
+#x = 1
+#y = 2
+#z = 3
+#v = 4
+
+#print(x, y, z, v)
+
+#x=1
+#y=1
+#z=1
+#v=1
+
+#x=y=z=v=1
+#print(z,y,x,v)
+
+#name = "My brother"
+#print(len(name))             #show number of letter
+#print(name.find("r"))        #find character wthin a string , start from 0
+#print(name.capitalize())     #capitalize the first letter
+#print(name.lower())          #lowercase first character
+#print(name.isdigit())        #Distinguish whether it is a number，true or false
+#print(name.isalpha())        #check whether the string are spaces
+#print(name.count('r'))       #(2) # count how many character are within the string
+#print(name.replace('r','x')) #change one letter to another letter
+#print(name*3)                 #display multi time
+
+#type casting = convert the data type of a value to another data type.
+
+#x=1    #int
+#y=2.0  #float
+#z='3'  #str
+
+#x=float(x)
+#y=int(y)
+#z=int(z)
+
+#print ("X is"+str(x))
+#print (y)
+#print (z*3)
+
+#name = input('what is your name?:')
+#age = int(input("how old are you?:"))
+#height = float(input('how tall are you?:'))
+
+#age = age + 1
+
+#print ("hello "+name)
+#print('you are '+str(age)+' years old')
+#print('you are '+str(height)+" cm tall")
+
+#import math
+import  math
+
+#pi = 3.14
+#x=2
+#y=1
+#print(round(pi))          #四舍五入
+#print(math.ceil(pi))      #向上取整数，12.2-13.0,12.7-13.0，12.0-12.0，10.0000000000000001F-10
+#print(abs(pi))             #绝对值
+#print(pow(pi,2))
+#print(math.sqrt(pi))
+#print(max(x,y))
+#print(min(x,y))
+#{[()]}
+
+#slicing = create a substring  by extracting elements from another string
+
+
+
+
+
+
